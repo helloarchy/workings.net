c38595a4768a9489ca9e6c4b5ce2bd42aefdfd82
Robert Hardy <helloarchy@gmail.com>
2021-04-26 20:02:49 +0100

    Use InputForm and built in Input types for forms

---
+++ Data/BlindModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Workings.Data
+{
+    public class BlindModel
+    {
+        [Required]
+        [StringLength(10, ErrorMessage = "Name is too long.")]
+        public string Client { get; set; }
+
+        [StringLength(10, ErrorMessage = "Name is too long.")]
+        public Customer Customer { get; set; }
+
+        [StringLength(10, ErrorMessage = "Name is too long.")]
+        public string Reference { get; set; }
+
+        public int BlindCount { get; set; }
+
+        public string RailingType { get; set; }
+    }
+
+    /* TODO: JFDI and use database!!! */
+    public enum Customer
+    {
+        BlindsNBlessings,
+        ChelseaFabrics,
+        CurtainFactoryOutlet,
+        FabricHouse,
+        JudyFletcher,
+        PosnerInteriors
+    }
+}
