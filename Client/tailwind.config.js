7cdc405524687549b7c8cb0da5c92676d8898d35
Robert Hardy <helloarchy@gmail.com>
2021-05-22 20:31:37 +0100

    Fix paperwork to aspect ration Section up paperwork Add details component for paperwork Add top calculations component for paperwork Add svg components to generate drawing

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,21 +1,22 @@
 module.exports = {
-  purge: {
-    enabled: true,
-    content: [
-      './**/*.html',
-      './**/*.razor'
+    purge: {
+        enabled: true,
+        content: [
+            './**/*.html',
+            './**/*.razor'
+        ],
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require("@tailwindcss/forms")({
+            strategy: 'class',
+        }),
+        require('@tailwindcss/aspect-ratio'),
     ],
-  },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require("@tailwindcss/forms")({
-      strategy: 'class',
-    }),
-  ],
 }
--- Shared/BlindModel.cs
+++ Shared/BlindModel.cs
@@ -8,16 +8,17 @@ namespace workings.Shared
     public class BlindModel
     {
         public int Id { get; set; }
-        
+
         [StringLength(10, ErrorMessage = "Name is too long.")]
-        public string Customer { get; set; }
+        public string Customer { get; set; } = "Hardy Family";
 
         [StringLength(25, MinimumLength = 4, ErrorMessage = "Name is too long.")]
-        public string Reference { get; set; }
-        public int CountBlinds { get; set; }
-        public decimal Width { get; set; }
-        public decimal Height { get; set; }
-        public int CountWidths { get; set; }
+        public string Reference { get; set; } = "Kids room";
+
+        public int CountBlinds { get; set; } = 1;
+        public double Width { get; set; } = 123.45;
+        public double Height { get; set; } = 123.45;
+        public int CountWidths { get; set; } = 1;
 
         [Range(1, 12, ErrorMessage = "The {0} are out of range (min {1}, max {2})")]
         public int Folds { get; set; } = 1;
